e9cea9ec89a8b9f6b479fba4d48f2d04f2f5ec92
Mohidworldwide <mohidssharif@gmail.com>
2024-11-16 20:49:26 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+# # file=open("xyz.txt")
+
+# # print(file.read())
+
+# # file.close()
+
+# file=open("xyz.txt","w")
+
+# file.write("hello!!!")
+
+
+# file=open("xyz.txt","r")
+
+# print(file.read())
+
+#activity 3 append
+file=open("xyz.txt","a")
+
+file.write("hiiiiiiiii")
+file=open("xyz.txt","r")
+
+print(file.read())
+
+
+file.close()
+
